96693e3b5f4297f33498a8d859f35eb4f08705d5
pogin <alle.sollen.mir.folgen.1378@gmail.com>
2016-01-31 07:09:46 +0900

    Solved

--- project-euler/problem4.hs
+++ project-euler/problem4.hs
@@ -1,13 +1,22 @@
-import Control.Applicative
 import Data.List
-import Control.Monad
+import Data.Ord (comparing)
 
+isPalindrome :: Int -> Bool
+isPalindrome xs = show xs == reverse (show xs)
 
+palindromeList :: [(Int, (Int, Int))]
+palindromeList =
+  let lst = [(x, y)| x <- [100..999], y <- [100..999]] :: [(Int, Int)]
+      tpl :: [(Int, (Int, Int))]
+      tpl = map (\x -> ((uncurry (\y z -> y * (toInt $ reverse $ show (z :: Int))) x), x)) lst
+      tplFilterd = filter (isPalindrome . fst) tpl
+  in sortBy (comparing fst) tplFilterd
+
+solve :: (Int, (Int, Int))
+solve = maximumBy (comparing fst) palindromeList
 
 toInt :: String -> Int
 toInt = read
 
 main :: IO ()
-main = do
-  let str = "123"
-  print str
+main = print $ fst solve
